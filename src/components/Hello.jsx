ecd550092c04a5ba24527f7aa9ee883bad0bbe14
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-10-27 04:23:46 +0500

    Fixing: Update .gitignore and Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,6 @@
 /**
  * Мини-компонент для демонстрации props.
- * Пример: <Hello user="John" />
+ * Пример: <Hello user="Aklima" />
  */
 export default function Hello({ user = "World" }) {
   return <h2 style={{ marginTop: 8 }}>Hello, {user}!</h2>;
